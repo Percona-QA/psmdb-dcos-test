112ede76952f18a53087bb7852c111acbb4310a5
Tomislav Plavcic <tomislav.plavcic@percona.com>
2018-08-24 12:38:15 +0200

    Remove python stuff for now

--- tests/test_durability.py
+++
@@ -1,9 +0,0 @@
-#/usr/bin/env python3
-import pytest
-from pymongo import MongoClient
-from general.general import DCOSTestGeneral
-
-dcos = DCOSTestGeneral()
-
-def test_prepare(check_replicaset_health):
-    assert check_replicaset_health

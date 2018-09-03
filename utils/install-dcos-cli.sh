0493ce1c9cc4ac1ba77f4df7b4964604b1cb7897
Tomislav Plavcic <tplavcic@gmail.com>
2018-09-03 10:00:17 -0400

    Small fix

--- utils/install-dcos-cli.sh
+++ utils/install-dcos-cli.sh
@@ -2,18 +2,18 @@
 
 source ../CONFIG
 
+if [ ${DCOS_TEST_VER#*-} = "ee" ]; then
+  CREDENTIALS="--username=bootstrapuser --password=deleteme"
+else
+  CREDENTIALS=""
+fi
+
 ${DCOS_CLI_BIN} cluster remove --all || true
 
 [ -d /usr/local/bin ] || sudo mkdir -p /usr/local/bin && 
 curl https://downloads.dcos.io/binaries/cli/linux/x86-64/dcos-${DCOS_TEST_VER%-*}/dcos -o dcos && 
 sudo mv dcos /usr/local/bin && 
 sudo chmod +x /usr/local/bin/dcos && 
-${DCOS_CLI_BIN} cluster setup ${CLUSTER_URL} --no-check && 
-if [ ${DCOS_TEST_VER#*-} = "ee" ]; then
-  CREDENTIALS="--username=bootstrapuser --password=deleteme"
-else
-  CREDENTIALS=""
-fi
 ${DCOS_CLI_BIN} cluster setup ${CLUSTER_URL} --no-check ${CREDENTIALS} && 
 ${DCOS_CLI_BIN}
 
